feb2c3a1f620548a29d3170ce91c336b71415434
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-27 22:00:39 -0600

    Add files via upload

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,20 @@
+import burgerMenu from '../assets/icon-hamburger.svg'
 
 
 
 export const Navbar = () => {
     return (
-        <nav>
+        <nav className='bg-blue-500 w-[329px] h-[305px]'>
             <ul>
                 <li>About</li>
                 <li>Services</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
+            <div>
+                <img src={burgerMenu} alt="" />
+            </div>
         </nav>
+
     )
 }
-
-
-About
-Services
-Projects
-Contact
