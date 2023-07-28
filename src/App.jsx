8251d112472a35deae769736227e653a9ae157d6
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-27 22:21:40 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/Header"
+import { Main } from "./components/Main"
+
 
 function App() {
 
   return (
     <>
-      <h1>Titulo de prueba</h1>
+      <Header />
+      <Main />
     </>
   )
 }
