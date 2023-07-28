10959d066bf854530d666a311829faa52489fbc5
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-27 22:21:57 -0600

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
-import logo from '../assets/logo.svg'
-import { Navbar } from '../Navbar'
-
+import logo from "../assets/logo.svg"
+import { Navbar } from "./Navbar"
 
 export const Header = () => {
     return (
-        <header className="bg-red-500">
+        <header className="relative">
             <img src={logo} alt="logo" />
+            <Navbar />
         </header>
     )
 }
