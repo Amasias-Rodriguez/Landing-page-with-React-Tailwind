851842e2e1dad4af31c03e5759948fc7826a1897
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-27 22:22:28 -0600

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,14 +4,15 @@ import burgerMenu from '../assets/icon-hamburger.svg'
 
 export const Navbar = () => {
     return (
-        <nav className='bg-blue-500 w-[329px] h-[305px]'>
-            <ul>
-                <li>About</li>
-                <li>Services</li>
-                <li>Projects</li>
-                <li>Contact</li>
+        <nav>
+            <ul className='bg-white w-[329px] h-[305px] absolute 
+        left-0 right-0 mx-auto top-[106px] flex flex-col items-center place-content-around'>
+                <li className='cursor-pointer'>About</li>
+                <li className='cursor-pointer'>Services</li>
+                <li className='cursor-pointer'>Projects</li>
+                <li className='cursor-pointer'>Contact</li>
             </ul>
-            <div>
+            <div className='bg-red-500'>
                 <img src={burgerMenu} alt="" />
             </div>
         </nav>
