c0a82e0901e2fd2036eacd7d15a190c1eefc2435
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-27 22:22:14 -0600

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,13 @@
-
+import imgMobile from '../assets/mobile/image-header.jpg'
+import imgDesktop from '../assets/desktop/image-header.jpg'
 
 export const Main = () => {
     return (
         <main>
             <picture>
-                <source />
+                <source media='(max-width:640px)' srcSet={imgMobile} />
+                <source media='(min-width:641px)' src={imgDesktop} />
+                <img src={imgMobile} alt="background header" />
             </picture>
         </main>
     )
