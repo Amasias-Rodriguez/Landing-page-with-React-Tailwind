7cb0b49965b8a17e3304d3fa73c7f48a7ca30212
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-29 00:25:02 -0600

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,14 +6,15 @@ export const Navbar = () => {
     return (
         <nav>
             <ul className='bg-white w-[329px] h-[305px] absolute 
-        left-0 right-0 mx-auto top-[106px] flex flex-col items-center place-content-around'>
+        left-0 right-0 mx-auto top-[106px] flex flex-col items-center place-content-around
+        after:content[""] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-t-transparent after:border-l-transparent after:border-b-white after:border-r-white'>
                 <li className='cursor-pointer'>About</li>
                 <li className='cursor-pointer'>Services</li>
                 <li className='cursor-pointer'>Projects</li>
                 <li className='cursor-pointer'>Contact</li>
             </ul>
-            <div className='bg-red-500'>
-                <img src={burgerMenu} alt="" />
+            <div className=''>
+                <img src={burgerMenu} alt="burgermenu" />
             </div>
         </nav>
 
