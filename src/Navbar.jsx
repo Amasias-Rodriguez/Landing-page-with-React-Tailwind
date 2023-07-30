d973e8e9e05539b7cb71e9ae4a77b6b9124c7c12
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-30 00:08:00 -0600

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,27 @@
 import burgerMenu from '../assets/icon-hamburger.svg'
+import { useState } from 'react'
 
 
 
 export const Navbar = () => {
+
+    const [menuClicked, setMenuClicked] = useState(false)
+
+    const handleClick = () => {
+        console.log('a click was made')
+    }
     return (
         <nav>
             <ul className='bg-white w-[329px] h-[305px] absolute 
-        left-0 right-0 mx-auto top-[106px] flex flex-col items-center place-content-around
-        after:content[""] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-t-transparent after:border-l-transparent after:border-b-white after:border-r-white'>
+        left-0 right-0 mx-auto top-[106px] flex-col items-center place-content-around
+        after:content[""] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-t-transparent after:border-l-transparent after:border-b-white after:border-r-white text-Dark-grayish-blue text-xl hidden'>
                 <li className='cursor-pointer'>About</li>
                 <li className='cursor-pointer'>Services</li>
                 <li className='cursor-pointer'>Projects</li>
-                <li className='cursor-pointer'>Contact</li>
+                <li className='cursor-pointer bg-Yellow w-[140px] h-[56px] grid place-content-center rounded-full font-Fraunces text-Very-dark-desatured-blue'>Contact</li>
             </ul>
-            <div className=''>
-                <img src={burgerMenu} alt="burgermenu" />
+            <div className='cursor-pointer' onClick={handleClick}>
+                <img src={burgerMenu} alt="menu hamburger" />
             </div>
         </nav>
 
