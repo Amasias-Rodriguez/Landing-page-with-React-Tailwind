90601616c20a57bde3409bf19945baf6e7268cc8
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-07-31 21:12:58 -0600

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,11 @@ import { Navbar } from "./Navbar"
 
 export const Header = () => {
     return (
-        <header className="absolute w-full px-6 pt-8 flex place-content-between font-Barlow">
-            <img src={logo} alt="logo" />
-            <Navbar />
+        <header className="absolute w-full px-6 pt-8 ">
+            <div className="flex place-content-between font-Barlow">
+                <img src={logo} alt="logo" />
+                <Navbar />
+            </div >
         </header>
     )
 }
