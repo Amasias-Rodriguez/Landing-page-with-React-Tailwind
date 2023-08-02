76cad21d6db35142e91b3cced1f968b80cdb1918
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-02 00:09:10 -0600

    Update Services.jsx

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,8 +1,36 @@
+import { Image } from "./Image"
+import { Text } from "./Text"
 
-export const Service = () => {
+import eggImgMobile from '../assets/mobile/image-transform.jpg'
+import eggImgDesktop from '../assets/desktop/image-transform.jpg'
+import glassImgMobile from '../assets/mobile/image-stand-out.jpg'
+import glassImgDesktop from '../assets/mobile/image-stand-out.jpg'
+
+export const Services = () => {
     return (
-        <section>
-
+        <section className="tablet:grid tablet:grid-cols-2">
+            <Image
+                imgMobile={eggImgMobile}
+                imgDesktop={eggImgDesktop}
+            />
+            <Text
+                title='Transform your brand'
+                text=' We are a full-service creative agency specializing in helping brands grow fast. 
+                Engage your clients through compelling visuals that do most of the marketing for you.'
+                color='bg-Yellow'
+            />
+            <Image
+                imgMobile={glassImgMobile}
+                imgDesktop={glassImgDesktop}
+            />
+            <Text
+                title='Stand out to the right audience'
+                text=' W  Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
+                color='bg-Sotf-red'
+            />
         </section>
     )
 }
+
+
+
