03ca816442e8cfd4aa8fc5037f780dcde8c94eb1
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-02 22:45:44 -0600

    Update Image.jsx

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,7 +1,7 @@
 
-export const Image = ({ imgMobile, imgDesktop }) => {
+export const Image = ({ order, imgMobile, imgDesktop }) => {
     return (
-        <picture>
+        <picture className={order}>
             <source media='(max-with:639px)' srcSet={imgMobile} />
             <source media='(min-with:640px)' srcSet={imgDesktop} />
             <img src={imgMobile} alt="image service" />
