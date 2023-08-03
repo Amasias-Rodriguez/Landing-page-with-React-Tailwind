319d35fbc1a5b4f723cdc80016fa7b42c3eb7d73
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-02 22:47:23 -0600

    Update Text.jsx

--- src/Text.jsx
+++ src/Text.jsx
@@ -1,12 +1,12 @@
 
-export const Text = ({ title, text, color }) => {
+export const Text = ({ order, title, text, color }) => {
     return (
-        <div className="text-center h-[437px] px-6 flex flex-col place-content-around py-6">
-            <h2 className="font-Fraunces text-[2rem] font-black tablet:text-[1.25rem]">{title}</h2>
-            <p className="font-Barlow text-Dark-grayish-blue text-[1.25rem] leading-[1.875rem] tablet:text-[0.8rem] tablet-leading-[1.5rem]" >{text}</p>
-            <button className="uppercase font-Fraunces">Learn More</button>
+        <div className={`${order} placeholder:text-center h-[437px] px-6 flex flex-col place-content-around py-6 tablet:h-auto lg:px-[65px] desktop:text-left desktop:pl-[165px] desktop:py-[140px] desktop:pr-[105px]`}>
+            <h2 className="font-Fraunces text-[2rem] font-black tablet:text-[1.25rem] lg:text-[2.5rem] desktop:leading-tight">{title}</h2>
+            <p className="font-Barlow text-Dark-grayish-blue text-[1.25rem] leading-[1.875rem] tablet:text-[0.8rem] tablet-leading-[1.5rem] lg:text-[18px]" >{text}</p>
+            <button className="uppercase font-Fraunces lg:text-1xl">Learn More</button>
             <div className="relative">
-                <div className={`w-[137px] h-[10px] ${color} rounded-full absolute mx-auto right-0 left-0 bottom-5 z-10`}></div>
+                <div className={`w-[137px] h-[10px] ${color} rounded-full absolute mx-auto right-0 left-0 bottom-5 z-10 bg-opacity-25 desktop:mx-0 desktop:left-[-10px]`}></div>
             </div>
         </div>
     )
