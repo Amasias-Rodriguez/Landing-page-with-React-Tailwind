ad7782f687510f4cf26e07f6b29f44a12fdf4480
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-02 22:46:15 -0600

    Update Services.jsx

--- src/Services.jsx
+++ src/Services.jsx
@@ -10,20 +10,24 @@ export const Services = () => {
     return (
         <section className="tablet:grid tablet:grid-cols-2">
             <Image
+                order='order-2'
                 imgMobile={eggImgMobile}
                 imgDesktop={eggImgDesktop}
             />
             <Text
+                order='order-1'
                 title='Transform your brand'
                 text=' We are a full-service creative agency specializing in helping brands grow fast. 
                 Engage your clients through compelling visuals that do most of the marketing for you.'
                 color='bg-Yellow'
             />
             <Image
+                order='order-3'
                 imgMobile={glassImgMobile}
                 imgDesktop={glassImgDesktop}
             />
             <Text
+                order='order-4'
                 title='Stand out to the right audience'
                 text=' W  Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
                 color='bg-Sotf-red'
