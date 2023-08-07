7cfd181d2f052844d541f6f2545f764d3cfadab6
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-06 23:05:46 -0600

    Update Testimonial.jsx

--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -1,11 +1,11 @@
 
 export const Testimonial = ({ img, testimonial, name, position }) => {
     return (
-        <div>
-            <img src={img} alt="photo" />
-            <p>{testimonial}</p>
-            <h3>{name}</h3>
-            <p>{position}</p>
+        <div className="flex flex-col items-center text-center mb-16">
+            <img className="w-[72px] rounded-full" src={img} alt="photo" />
+            <p className="my-8 font-Barlow text-Very-dark-grayish-blue px-4 max-w-[400px]">{testimonial}</p>
+            <h3 className="font-Fraunces text-[18px] text-Very-dark-grayish-blue">{name}</h3>
+            <p className="text-Grayish-blue font-Barlow text-[14px]">{position}</p>
         </div>
     )
 }
