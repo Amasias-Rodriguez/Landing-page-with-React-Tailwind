feb50be2fe8a7cb4658207063e47ef960ca63bbc
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-08-07 22:17:40 -0600

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.svg';
+export const Footer = () => {
+    return (
+        <footer>
+            <img src={logo} alt="logo" />
+        </footer>
+    )
+}
+
+export default Footer
